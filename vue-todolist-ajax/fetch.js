f5f93734444ebc1dd54bb77b8408da628a66fef9
geekxia <448914712@qq.com>
2020-06-10 16:58:42 +0800

    1

--- vue-todolist-ajax/fetch.js
+++ vue-todolist-ajax/fetch.js
@@ -1,6 +1,7 @@
 var baseUrl = 'http://10.36.147.164:9999'
 
 function fetch(url, method, data, callback) {
+  data.userId = 'sz191613035'
   $.ajax({
     url: baseUrl+url,
     method: method,
--- vue-todolist-ajax/todo.js
+++ vue-todolist-ajax/todo.js
@@ -12,14 +12,14 @@ var app = new Vue({
     confirm() {
       var that = this
       if (!this.todo) return
-      fetch('/todo/addTodo', 'POST', {userId: 'sz191613035', task: this.todo}, function() {
+      fetch('/todo/addTodo', 'POST', {task: this.todo}, function() {
         that.todo = ''
         that.getTodoList()
       })
     },
     remove(item) {
       var that = this
-      fetch('/todo/deleteTodo', 'GET', {userId:'sz191613035', id: item._id}, function() {
+      fetch('/todo/deleteTodo', 'GET', {id: item._id}, function() {
         that.getTodoList()
       })
     },
@@ -29,26 +29,20 @@ var app = new Vue({
       if (type === 'up') {
         status = '1'
       }
-      fetch('/todo/changeTodoStatus', 'GET', {userId:'sz191613035', id:item._id, status:status}, function(){
+      fetch('/todo/changeTodoStatus', 'GET', {id:item._id, status:status}, function(){
         that.getTodoList()
       })
     },
     edit(item,idx,type) {
       var that = this
-      var newTask = ''
-      if (type=='up') {
-        newTask = that.data.undone[idx].task
-      } else {
-        newTask = that.data.done[idx].task
-      }
-      fetch('/todo/editTodo', 'POST', {userId:'sz191613035',id:item._id,task:newTask}, function() {
+      fetch('/todo/editTodo', 'POST', {id:item._id,task:item.task}, function() {
         that.getTodoList()
       })
     },
     // 第次页面刷新时，调用该方法
     getTodoList() {
       var that = this
-      fetch('/todo/getMyTodos', 'GET', {userId: 'sz191613035'}, function(res) {
+      fetch('/todo/getMyTodos', 'GET', {}, function(res) {
         that.data = res.data
         console.log('this', that.data)
       })
